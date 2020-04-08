ff66c70759215fe0c204a4e152d1b2a8cda620d8
Tom Nicholas <tom_here@yahoo.com>
2020-04-08 11:53:14 -0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 import './styles.css';
 
 export default function App() {
@@ -10,9 +11,15 @@ export default function App() {
 }
 
 const TicTacToe = () => {
-  const [win, setWin] = useState({ win: false, char: '' });
-  const [current, setCurrent] = useState('X');
-  const [board, setBoard] = useState('         ');
+  //const [win, setWin] = useState({ win: false, char: '' });
+  // const [current, setCurrent] = useState('X');
+  //  const [board, setBoard] = useState('         ');
+  const [game, setGame] = useState({
+    win: false,
+    char: '',
+    current: 'X',
+    board: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  });
 
   useEffect(() => {
     function handleResize() {
@@ -20,10 +27,8 @@ const TicTacToe = () => {
       var [t1] = document.getElementsByClassName('tictactoe-game');
       var t2 = document.getElementsByClassName('tictactoe-square-text');
       var [t3] = document.getElementsByClassName('win-box');
-      var width = window.innerWidth;
-      var height = window.innerHeight;
       var t;
-      var m = width > height ? 'vh' : 'vw';
+      var m = window.innerWidth > window.innerHeight ? 'vh' : 'vw';
       t1.style.height = t1.style.width = '95' + m;
       for (t of t2) t.style.fontSize = '25' + m;
       if (t3) {
@@ -37,80 +42,76 @@ const TicTacToe = () => {
   });
 
   const handleClick = n => {
-    if (board[n] !== ' ') return; // move already made
-    let t = [...board];
-    t[n] = current;
-    t = t.join('');
-    setCurrent(current === 'X' ? 'O' : 'X');
-    setBoard(t);
-    checkBoard(t);
+    if (game.board[n] !== ' ') return; // move already made
+    let t = [...game.board];
+    t[n] = game.current;
+    let { win, char } = checkBoard(t);
+    console.log(win, char);
+    setGame({
+      win: win,
+      char: char,
+      current: game.current === 'X' ? 'O' : 'X',
+      board: t
+    });
   };
 
   const checkBoard = s => {
     for (let m of ['X', 'O']) {
       for (let i = 0; i < 3; i++) {
         if (s[i * 3] === m && s[i * 3 + 1] === m && s[i * 3 + 2] === m)
-          setWin({ win: true, char: m });
+          return { win: true, char: m };
       }
       for (let i = 0; i < 3; i++) {
         if (s[i] === m && s[i + 3] === m && s[i + 6] === m)
-          setWin({ win: true, char: m });
+          return { win: true, char: m };
       }
-      if (s[0] === m && s[4] === m && s[8] === m)
-        setWin({ win: true, char: m });
-      if (s[6] === m && s[4] === m && s[2] === m)
-        setWin({ win: true, char: m });
+      if (s[0] === m && s[4] === m && s[8] === m) return { win: true, char: m };
+      if (s[6] === m && s[4] === m && s[2] === m) return { win: true, char: m };
     }
-    if (!s.includes(' ')) setWin({ win: true, char: '😺' });
+    if (!s.includes(' ')) return { win: true, char: '😺' };
+    return { win: false, char: '' };
   };
 
   const resetBoard = () => {
-    setWin({ win: false, char: '' });
-    setCurrent('X');
-    setBoard('         ');
+    setGame({
+      win: false,
+      char: '',
+      current: 'X',
+      board: '         '
+    });
   };
+
+  const Blank = <div className="tictactoe-blank" />;
 
   return (
     <div className="tictactoe-game">
       <div className="tictactoe-container">
-        <Square val={board[0]} onClick={n => handleClick(0)} />
-        <div className="tictactoe-blank" />
-        <Square val={board[1]} onClick={n => handleClick(1)} />
-        <div className="tictactoe-blank" />
-        <Square val={board[2]} onClick={n => handleClick(2)} />
-
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-
-        <Square val={board[3]} onClick={n => handleClick(3)} />
-        <div className="tictactoe-blank" />
-        <Square val={board[4]} onClick={n => handleClick(4)} />
-        <div className="tictactoe-blank" />
-        <Square val={board[5]} onClick={n => handleClick(5)} />
-
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-        <div className="tictactoe-blank" />
-
-        <Square val={board[6]} onClick={n => handleClick(6)} />
-        <div className="tictactoe-blank" />
-        <Square val={board[7]} onClick={n => handleClick(7)} />
-        <div className="tictactoe-blank" />
-        <Square val={board[8]} onClick={n => handleClick(8)} />
+        <Square val={game.board[0]} click={n => handleClick(0)} />
+        {Blank}
+        <Square val={game.board[1]} click={n => handleClick(1)} />
+        {Blank}
+        <Square val={game.board[2]} click={n => handleClick(2)} />
+        {_.times(5, () => Blank)}
+        <Square val={game.board[3]} click={n => handleClick(3)} />
+        {Blank}
+        <Square val={game.board[4]} click={n => handleClick(4)} />
+        {Blank}
+        <Square val={game.board[5]} click={n => handleClick(5)} />
+        {_.times(5, () => Blank)}
+        <Square val={game.board[6]} click={n => handleClick(6)} />
+        {Blank}
+        <Square val={game.board[7]} click={n => handleClick(7)} />
+        {Blank}
+        <Square val={game.board[8]} click={n => handleClick(8)} />
       </div>
-      {win.win ? <Win char={win.char} reset={resetBoard} /> : ''}
+      {game.win ? <Win char={game.char} reset={resetBoard} /> : ''}
     </div>
   );
 };
 
-const Square = ({ val, onClick }) => {
+const Square = ({ val, click }) => {
   return (
-    <div className="tictactoe-square" onClick={onClick}>
+    <div className="tictactoe-square" onClick={click}>
       <div className="tictactoe-square-text">{val}</div>
     </div>
   );
