fc93a97365109fdede0661537c6fc389ce577396
Tom Nicholas <tom_here@yahoo.com>
2020-04-08 09:24:15 -0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import "./styles.css";
+import React, { useState, useEffect } from 'react';
+import './styles.css';
 
 export default function App() {
   return (
@@ -10,56 +10,56 @@ export default function App() {
 }
 
 const TicTacToe = () => {
-  const [win, setWin] = useState({ win: false, char: "" });
-  const [current, setCurrent] = useState("X");
-  const [board, setBoard] = useState("         ");
+  const [win, setWin] = useState({ win: false, char: '' });
+  const [current, setCurrent] = useState('X');
+  const [board, setBoard] = useState('         ');
 
   useEffect(() => {
     function handleResize() {
       // fit game to fill viewport
-      var t1 = document.getElementsByClassName("tictactoe-game");
-      var t2 = document.getElementsByClassName("tictactoe-square-text");
-      var t3 = document.getElementsByClassName("win-box");
+      var t1 = document.getElementsByClassName('tictactoe-game');
+      var t2 = document.getElementsByClassName('tictactoe-square-text');
+      var t3 = document.getElementsByClassName('win-box');
       var width = window.innerWidth;
       var height = window.innerHeight;
       var t;
       if (width > height) {
-        t1[0].style.height = "90vh";
-        t1[0].style.width = "90vh";
-        for (t of t2) t.style.fontSize = "25vh";
-        t2[0].style.fontSize = "25vh";
+        t1[0].style.height = '90vh';
+        t1[0].style.width = '90vh';
+        for (t of t2) t.style.fontSize = '25vh';
+        t2[0].style.fontSize = '25vh';
         if (t3.length) {
-          t3[0].style.fontSize = "28vh";
-          t3[0].style.lineHeight = "32vh";
-          t3[0].style.paddingTop = "16vh";
+          t3[0].style.fontSize = '28vh';
+          t3[0].style.lineHeight = '32vh';
+          t3[0].style.paddingTop = '16vh';
         }
       } else {
-        t1[0].style.height = "90vw";
-        t1[0].style.width = "90vw";
-        for (t of t2) t.style.fontSize = "25vw";
+        t1[0].style.height = '90vw';
+        t1[0].style.width = '90vw';
+        for (t of t2) t.style.fontSize = '25vw';
         if (t3.length) {
-          t3[0].style.fontSize = "28vw";
-          t3[0].style.lineHeight = "32vw";
-          t3[0].style.paddingTop = "16vw";
+          t3[0].style.fontSize = '28vw';
+          t3[0].style.lineHeight = '32vw';
+          t3[0].style.paddingTop = '16vw';
         }
       }
     }
     handleResize();
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
   });
 
   const handleClick = n => {
-    if (board[n] !== " ") return; // move already made
+    if (board[n] !== ' ') return; // move already made
     let t = [...board];
     t[n] = current;
-    t = t.join("");
-    setCurrent(current === "X" ? "O" : "X");
+    t = t.join('');
+    setCurrent(current === 'X' ? 'O' : 'X');
     setBoard(t);
     setTimeout(() => checkBoard(t), 100);
   };
 
   const checkBoard = s => {
-    for (let m of ["X", "O"]) {
+    for (let m of ['X', 'O']) {
       for (let i = 0; i < 3; i++) {
         if (s[i * 3] === m && s[i * 3 + 1] === m && s[i * 3 + 2] === m)
           setWin({ win: true, char: m });
@@ -73,13 +73,13 @@ const TicTacToe = () => {
       if (s[6] === m && s[4] === m && s[2] === m)
         setWin({ win: true, char: m });
     }
-    if (!s.includes(" ")) setWin({ win: true, char: "😺" });
+    if (!s.includes(' ')) setWin({ win: true, char: '😺' });
   };
 
   const resetBoard = () => {
-    setWin({ win: false, char: "" });
-    setCurrent("X");
-    setBoard("         ");
+    setWin({ win: false, char: '' });
+    setCurrent('X');
+    setBoard('         ');
   };
 
   return (
@@ -115,7 +115,7 @@ const TicTacToe = () => {
         <div className="tictactoe-blank" />
         <Square val={board[8]} onClick={n => handleClick(8)} />
       </div>
-      {win.win ? <Win char={win.char} reset={resetBoard} /> : ""}
+      {win.win ? <Win char={win.char} reset={resetBoard} /> : ''}
     </div>
   );
 };
